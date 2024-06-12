8c4059d966c5a7aac4dc41cbd2bef8422067f49d
zoubi <jean-luc@bloechle.ch>
2024-06-12 12:46:42 +0200

    DocEng'24 version 1.0.8

---
+++ src/zig/zag/util/ArgsParser.java
@@ -0,0 +1,74 @@
+package zig.zag.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsParser {
+    private final Map<String, String> argsMap = new HashMap<>();
+
+    public ArgsParser(String[] args) {
+        parseArgs(args);
+    }
+
+    private void parseArgs(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].startsWith("-")) {
+                String key = args[i];
+                if (i + 1 < args.length && !args[i + 1].startsWith("-")) {
+                    String value = parseValue(args, ++i);
+                    argsMap.put(key, value);
+                } else {
+                    argsMap.put(key, "true");
+                }
+            }
+        }
+    }
+
+    private String parseValue(String[] args, int index) {
+        StringBuilder value = new StringBuilder(args[index]);
+        if (value.charAt(0) == '"') {
+            value = new StringBuilder(value.substring(1));
+            while (index + 1 < args.length && !args[index + 1].endsWith("\"")) {
+                value.append(" ").append(args[++index]);
+            }
+            if (index + 1 < args.length && args[index + 1].endsWith("\"")) {
+                value.append(" ").append(args[++index], 0, args[index].length() - 1);
+            }
+        }
+        return value.toString();
+    }
+
+    public boolean has(String key) {
+        return argsMap.containsKey(key);
+    }
+
+    public String get(String key) {
+        return argsMap.get(key);
+    }
+
+    public int getInt(String key, int defaultValue) {
+        return argsMap.containsKey(key) ? Integer.parseInt(argsMap.get(key)) : defaultValue;
+    }
+
+    public String get(String key, String defaultValue) {
+        return argsMap.getOrDefault(key, defaultValue);
+    }
+
+    public boolean getBoolean(String key, boolean defaultValue) {
+        return argsMap.containsKey(key) ? Boolean.parseBoolean(argsMap.get(key)) : defaultValue;
+    }
+
+    public void printUsage() {
+        System.out.println("Usage:");
+        System.out.println("  java Main [-size <size>] [-percent <percent>] [-mode <mode>] [-threads <threads>] [-exit <true/false>] -input <inputFilePath> [-output <outputFilePath>] [-debug <true/false>]");
+        System.out.println("    -input <inputFilePath>   : Path to the input image file (required)");
+        System.out.println("    -output <outputFilePath> : Path to the output image file (optional, default is inputFilePath with 'ZZ' postfix)");
+        System.out.println("    -size <size>             : Window size (optional, default is 30)");
+        System.out.println("    -percent <percent>       : Mean weight for historical documents (optional, default is 100)");
+        System.out.println("    -mode <mode>             : Processing mode (optional, default is 1, 0=binary, 1=binary upsampled, 2=binary antialiased, 3=gray, 4=color)");
+        System.out.println("    -threads <threads>       : Number of threads for processing (optional, default is half the number of available processors)");
+        System.out.println("    -lossless <true/false>   : Enable or disable lossless compression (optional, default is true)");
+        System.out.println("    -debug <true/false>      : Enable or disable debug mode (optional, default is false)");
+        System.out.println("    -exit <true/false>       : Whether to exit the application after processing (optional, default is false)");
+    }
+}
---
+++ src/zig/zag/util/ImageUtil.java
@@ -0,0 +1,167 @@
+package zig.zag.util;
+
+import zig.zag.process.SliceProcessThreadPool;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.*;
+
+public abstract class ImageUtil {
+    public static BufferedImage createGrayImage(int width, int height) {
+        return new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+    }
+
+    public static BufferedImage convertToGray(BufferedImage rgbImg, BufferedImage grayImg) {
+        if (grayImg == null)
+            grayImg = createGrayImage(rgbImg.getWidth(), rgbImg.getHeight());
+        Graphics g = grayImg.getGraphics();
+        g.drawImage(rgbImg, 0, 0, null);
+        g.dispose();
+        return grayImg;
+    }
+
+    public static BufferedImage convertToRGBIfNeeded(BufferedImage img) {
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        switch (img.getType()) {
+            case BufferedImage.TYPE_INT_RGB:
+            case BufferedImage.TYPE_INT_BGR:
+            case BufferedImage.TYPE_3BYTE_BGR:
+                break;
+            default:
+                System.out.println("ImageUtil - converting to RGB from type=" + img.getType());
+                BufferedImage rgbImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+                rgbImage.createGraphics().drawImage(img, 0, 0, width, height, null);
+                img = rgbImage;
+                break;
+        }
+        return img;
+    }
+
+    public static int[] getHistogram(WritableRaster raster, int band, int marginPercent) {
+        int h = raster.getHeight();
+        int w = raster.getWidth();
+        int marginH = h * marginPercent / 100;
+        int marginW = w * marginPercent / 100;
+        h -= marginH;
+        w -= marginW;
+        int[] histogram = new int[256];
+        for (int y = marginH; y < h; y++)
+            for (int x = marginW; x < w; x++)
+                histogram[raster.getSample(x, y, band)]++;
+        return histogram;
+    }
+
+    public static int computeOtsuThreshold(int[] histogram) {
+        int total = histogram.length;
+        float maxBetween = 0;
+        int threshold = 0;
+
+        for (int i = 0; i < total; i++) {
+            float sumB = 0, meanB = 0;
+            for (int j = 0; j < i; j++) {
+                sumB += histogram[j];
+                meanB += histogram[j] * j;
+            }
+            float sumF = 0, meanF = 0;
+            for (int j = i; j < total; j++) {
+                sumF += histogram[j];
+                meanF += histogram[j] * j;
+            }
+
+            meanB /= sumB;
+            meanF /= sumF;
+
+            if (sumB > 0 && sumF > 0) {
+                float between = sumB * sumF * (meanF - meanB) * (meanF - meanB);
+                if (between > maxBetween) {
+                    maxBetween = between;
+                    threshold = i;
+                }
+            }
+        }
+        return threshold;
+    }
+
+    public static BufferedImage scaleImage(BufferedImage srcImg, double scale, boolean bicubic, BufferedImage scaleImg) {
+        if (scaleImg == null)
+            scaleImg = new BufferedImage((int) Math.round(srcImg.getWidth() * scale), (int) Math.round(srcImg.getHeight() * scale), srcImg.getType());
+        AffineTransform transform = new AffineTransform();
+        transform.scale(scale, scale);
+        new AffineTransformOp(transform, bicubic ? AffineTransformOp.TYPE_BICUBIC : AffineTransformOp.TYPE_BILINEAR).filter(srcImg, scaleImg);
+        return scaleImg;
+    }
+
+    public static void decimateImage(BufferedImage upImg, BufferedImage gImg, boolean gammaCorrection, SliceProcessThreadPool threadPool) {
+        if (threadPool == null)
+            threadPool = SliceProcessThreadPool.EMPTY_THREAD_POOL;
+
+        WritableRaster upRaster = upImg.getRaster();
+        WritableRaster grayRaster = gImg.getRaster();
+        int width = gImg.getWidth();
+        int height = gImg.getHeight();
+
+        int maxX = width - 1;
+        int maxY = height - 1;
+
+        threadPool.execute(height, (threadIndex, startY, endY) -> {
+            int[] grayValues = new int[9];
+            for (int y = startY; y < endY; y++) {
+                for (int x = 0; x < width; x++) {
+                    int xUp = 2 * x - 1;
+                    int yUp = 2 * y - 1;
+
+                    if (x == 0) xUp++;
+                    else if (x == maxX) xUp--;
+
+                    if (y == 0) yUp++;
+                    else if (y == maxY) yUp--;
+
+                    int sum = 0;
+                    for (int value : upRaster.getSamples(xUp, yUp, 3, 3, 0, grayValues))
+                        sum += value;
+
+                    int mean = sum / 9;
+
+                    if (gammaCorrection)
+                        mean = (int) (255 * Math.pow(mean / 255.0, 1.5));
+
+                    grayRaster.setSample(x, y, 0, mean);
+                }
+            }
+        });
+    }
+
+    public static void writeImage(BufferedImage img, File file) {
+        writeImage(img, file, null);
+    }
+
+    public static void writeImage(BufferedImage img, File file, String suffix) {
+        if (img != null && file != null) {
+            if (suffix != null && !suffix.isEmpty())
+                file = new File(removeExtension(file.getPath()) + (suffix.contains(".") ? suffix : suffix + ".png"));
+
+            try {
+                ImageIO.write(img, file.getPath().contains(".png") ? "png" : "jpg", file);
+            } catch (IOException e) {
+                printStackTrace(e);
+            }
+        }
+    }
+
+    public static String removeExtension(String path) {
+        int i = path.lastIndexOf(".");
+        return i > 0 ? path.substring(0, i) : path;
+    }
+
+    public static void printStackTrace(Exception e)
+    {
+        System.out.println("Exception thrown: "+e.getMessage());
+        e.printStackTrace();
+    }
+}
